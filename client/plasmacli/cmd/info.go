585587e32df638f7595d3af7f09cf59bc5a4d762
Colin Axner <colinaxner@berkeley.edu>
2018-11-17 11:55:53 -0800

    added info command, reverted balance cmd

--- client/plasmacli/cmd/info.go
+++ client/plasmacli/cmd/info.go
@@ -5,20 +5,20 @@ import (
 	"github.com/FourthState/plasma-mvp-sidechain/client"
 	"github.com/FourthState/plasma-mvp-sidechain/client/context"
 	"github.com/FourthState/plasma-mvp-sidechain/types"
+	"github.com/FourthState/plasma-mvp-sidechain/utils"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	rootCmd.AddCommand(balanceCmd)
-	balanceCmd.Flags().String(client.FlagNode, "tcp://localhost:26657", "<host>:<port> to tendermint rpc interface for this chain")
+	rootCmd.AddCommand(infoCmd)
+	infoCmd.Flags().String(client.FlagNode, "tcp://localhost:26657", "<host>:<port> to tendermint rpc interface for this chain")
 
 }
 
-var balanceCmd = &cobra.Command{
-	Use:   "balance <address>",
-	Short: "Query Balances",
-	Long:  "Query Balances",
+var infoCmd = &cobra.Command{
+	Use:   "info <address>",
+	Short: "Information on owned utxos ",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx := context.NewClientContextFromViper()
 
@@ -35,9 +35,19 @@ var balanceCmd = &cobra.Command{
 			if err != nil {
 				return err
 			}
-			fmt.Printf("Position: %v \nAmount: %d \n", utxo.Position, utxo.Amount)
+			fmt.Printf("\nPosition: %v \nAmount: %d \n", utxo.Position, utxo.Amount)
+			inputAddrHelper(utxo)
 		}
 
 		return nil
 	},
+}
+
+func inputAddrHelper(utxo types.BaseUTXO) {
+	if utxo.Position.DepositNum == 0 {
+		fmt.Printf("First Input Address: %s \n", utxo.InputAddresses[0].Hex())
+		if !utils.ZeroAddress(utxo.InputAddresses[1]) {
+			fmt.Printf("Second Input Address: %s \n", utxo.InputAddresses[1].Hex())
+		}
+	}
 }
