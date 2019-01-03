259886fa62174976e7835d653198881993582f30
Hamdi Allam <hamdi.allam97@gmail.com>
2019-01-02 18:27:49 -0800

    eth module does not persist any information and queries geth node instead

--- eth/plasma.go
+++ eth/plasma.go
@@ -1,7 +1,6 @@
 package eth
 
 import (
-	"context"
 	"crypto/ecdsa"
 	"fmt"
 	contracts "github.com/FourthState/plasma-mvp-sidechain/contracts/wrappers"
@@ -9,10 +8,6 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/rlp"
-	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/comparer"
-	"github.com/syndtr/goleveldb/leveldb/memdb"
 	"github.com/tendermint/tendermint/libs/log"
 	"math/big"
 	"sync"
@@ -20,132 +15,104 @@ import (
 
 // Plasma holds related unexported members
 type Plasma struct {
-	session *contracts.PlasmaMVPSession
-	client  Client
-	logger  log.Logger
+	*sync.Mutex
+	operatorSession *contracts.PlasmaMVPSession
+	contract        *contracts.PlasmaMVP
 
-	memdb *memdb.DB
-	db    *leveldb.DB
+	client Client
+	logger log.Logger
 
 	blockNum      *big.Int
 	ethBlockNum   *big.Int
 	finalityBound uint64
-
-	lock *sync.Mutex
 }
 
 // InitPlasma binds the go wrapper to the deployed contract. This private key provides authentication for the operator
-func InitPlasma(contractAddr common.Address, privateKey *ecdsa.PrivateKey, client Client, finalityBound uint64, logger log.Logger) (Plasma, error) {
+func InitPlasma(contractAddr common.Address, client Client, finalityBound uint64, logger log.Logger, isOperator bool, operatorPrivKey *ecdsa.PrivateKey) (*Plasma, error) {
 	plasmaContract, err := contracts.NewPlasmaMVP(contractAddr, client.ec)
 	if err != nil {
-		return Plasma{}, err
+		return nil, err
 	}
 
 	// Create a session with the contract and operator account
-	auth := bind.NewKeyedTransactor(privateKey)
-	plasmaSession := &contracts.PlasmaMVPSession{
-		Contract: plasmaContract,
-		CallOpts: bind.CallOpts{
-			Pending: true,
-		},
-		TransactOpts: bind.TransactOpts{
-			From:     auth.From,
-			Signer:   auth.Signer,
-			GasLimit: 3141592, // aribitrary. TODO: check this
-		},
+	var operatorSession *contracts.PlasmaMVPSession = nil
+	if isOperator {
+		auth := bind.NewKeyedTransactor(operatorPrivKey)
+		operatorSession = &contracts.PlasmaMVPSession{
+			Contract: plasmaContract,
+			CallOpts: bind.CallOpts{
+				Pending: true,
+			},
+			TransactOpts: bind.TransactOpts{
+				From:     auth.From,
+				Signer:   auth.Signer,
+				GasLimit: 3141592, // aribitrary. TODO: check this
+			},
+		}
 	}
 
 	// TODO: deal with syncing issues
-	lastCommittedBlock, err := plasmaSession.LastCommittedBlock()
+	lastCommittedBlock, err := plasmaContract.LastCommittedBlock(nil)
 	if err != nil {
-		return Plasma{}, fmt.Errorf("Contract session not correctly established - %s", err)
+		return nil, fmt.Errorf("Contract connection not correctly established - %s", err)
 	}
 
-	plasma := Plasma{
-		session: plasmaSession,
-		client:  client,
-		// capacity argument is advisory and not enforced in the memdb implementation
-		// TODO: flush the in-memory DB to a local one to bound memory consumption
-		memdb:  memdb.New(comparer.DefaultComparer, 1),
-		logger: logger,
+	plasma := &Plasma{
+		operatorSession: operatorSession,
+		contract:        plasmaContract,
+		client:          client,
+		logger:          logger,
 
 		ethBlockNum:   big.NewInt(-1),
 		blockNum:      lastCommittedBlock,
 		finalityBound: finalityBound,
 
-		lock: &sync.Mutex{},
+		Mutex: &sync.Mutex{},
 	}
 
 	// listen to new ethereum block headers
 	ethCh, err := client.SubscribeToHeads()
 	if err != nil {
 		logger.Error("Could not successfully subscribe to heads: %v", err)
-		return Plasma{}, err
+		return nil, err
 	}
 
-	// start listeners
 	go watchEthBlocks(plasma, ethCh)
-	go watchDeposits(plasma)
-	go watchExits(plasma)
 
 	return plasma, nil
 }
 
 // SubmitBlock proxy. TODO: handle batching with a timmer interrupt
-func (plasma Plasma) SubmitBlock(block plasmaTypes.Block) (*types.Transaction, error) {
+func (plasma *Plasma) SubmitBlock(block plasmaTypes.Block) error {
 	plasma.blockNum = plasma.blockNum.Add(plasma.blockNum, big.NewInt(1))
 
-	tx, err := plasma.session.SubmitBlock(
+	_, err := plasma.operatorSession.SubmitBlock(
 		[][32]byte{block.Header},
 		[]*big.Int{big.NewInt(int64(block.TxnCount))},
 		[]*big.Int{block.TotalFee},
 		plasma.blockNum)
 
-	if err != nil {
-		return nil, err
-	}
-
-	return tx, nil
+	return err
 }
 
 // GetDeposit checks the existence of a deposit nonce
-func (plasma Plasma) GetDeposit(nonce *big.Int) (plasmaTypes.Deposit, bool) {
-	key := prefixKey(depositPrefix, nonce.Bytes())
-	data, err := plasma.memdb.Get(key)
-
-	var deposit plasmaTypes.Deposit
-	// check against the contract if the deposit is not in the cache or decoding fails
-	if err != nil || rlp.DecodeBytes(data, &deposit) != nil {
-		if plasma.memdb.Contains(key) {
-			plasma.logger.Info("corrupted deposit found within db")
-			plasma.memdb.Delete(key)
-		}
-
-		deposit, err := plasma.session.Deposits(nonce)
-		if err != nil {
-			plasma.logger.Error("contract call, deposits, failed")
-			return plasmaTypes.Deposit{}, false
-		}
-
-		if deposit.CreatedAt.Sign() == 0 {
-			return plasmaTypes.Deposit{}, false
-		}
-
-		// save to the db
-		data, err = rlp.EncodeToBytes(deposit)
-		if err != nil {
-			plasma.logger.Error("error encoding deposit. will not be cached")
-		} else {
-			plasma.memdb.Put(key, data)
-		}
+func (plasma *Plasma) GetDeposit(nonce *big.Int) (plasmaTypes.Deposit, bool) {
+	deposit, err := plasma.contract.Deposits(nil, nonce)
+	if err != nil {
+		// TODO: log the error
+		return plasmaTypes.Deposit{}, false
 	}
 
-	// check finality bound for the deposit
-	plasma.lock.Lock()
+	if deposit.CreatedAt.Sign() == 0 {
+		return plasmaTypes.Deposit{}, false
+	}
+
+	// check the finality bound
+	plasma.Lock()
 	ethBlockNum := plasma.ethBlockNum
-	plasma.lock.Unlock()
+	plasma.Unlock()
 	if ethBlockNum.Sign() < 0 {
-		plasma.logger.Error("Failed `GetDeposit`. Not subscribed to ethereum headers")
+		plasma.logger.Error("failed `GetDeposit`. not subscribed to ethereum headers")
 		return plasmaTypes.Deposit{}, false
 	}
 
@@ -153,14 +120,15 @@ func (plasma Plasma) GetDeposit(nonce *big.Int) (plasmaTypes.Deposit, bool) {
 		return plasmaTypes.Deposit{}, false
 	}
 
-	return deposit, true
+	return plasmaTypes.Deposit{
+		Owner:       deposit.Owner,
+		Amount:      deposit.Amount,
+		EthBlockNum: deposit.EthBlockNum,
+	}, true
 }
 
 // HasTXBeenExited indicates if the position has ever been exited
-func (plasma Plasma) HasTxBeenExited(position plasmaTypes.Position) bool {
-	var key []byte
-	priority := position.Priority()
-
+func (plasma *Plasma) HasTxBeenExited(position plasmaTypes.Position) bool {
 	type exit struct {
 		Amount       *big.Int
 		CommittedFee *big.Int
@@ -169,109 +137,35 @@ func (plasma Plasma) HasTxBeenExited(position plasmaTypes.Position) bool {
 		State        uint8
 	}
 
-	if !plasma.memdb.Contains(key) {
-		var e exit
-		var err error
-		if position.IsDeposit() {
-			e, err = plasma.session.DepositExits(priority)
-		} else {
-			e, err = plasma.session.TxExits(priority)
-		}
+	var (
+		e   exit
+		err error
+	)
 
-		// default to true if the contract cannot be queried. Nothing should be spent
-		if err != nil {
-			plasma.logger.Error(fmt.Sprintf("Error querying contract %s", err))
-			return true
-		}
-
-		if e.State == 1 || e.State == 3 {
-			return true
-		} else {
-			return false
-		}
+	priority := position.Priority()
+	if position.IsDeposit() {
+		e, err = plasma.contract.DepositExits(nil, priority)
+	} else {
+		e, err = plasma.contract.TxExits(nil, priority)
 	}
 
-	return true
+	if err != nil {
+		// TODO: log the error
+		return true // censor spends until resolved
+	}
+
+	return e.State == 1 || e.State == 3
 }
 
-func watchDeposits(plasma Plasma) {
-	// suscribe to future deposits
-	deposits := make(chan *contracts.PlasmaMVPDeposit)
-	opts := &bind.WatchOpts{
-		Start:   nil, // latest block
-		Context: context.Background(),
-	}
-	plasma.session.Contract.WatchDeposit(opts, deposits)
+// keep track of the probabilistic latest ethereum state (reorgs) for finality purposes
+func watchEthBlocks(plasma *Plasma, ch <-chan *types.Header) {
+	plasma.logger.Info("listening to ethereum block headers")
 
-	for deposit := range deposits {
-		key := prefixKey(depositPrefix, deposit.DepositNonce.Bytes())
-
-		// remove the nonce, encode, and store
-		data, err := rlp.EncodeToBytes(&plasmaTypes.Deposit{deposit.Depositor, deposit.Amount, deposit.EthBlockNum})
-
-		if err != nil {
-			plasma.logger.Error("Error encoding deposit event from contract -", deposit)
-		} else {
-			plasma.memdb.Put(key, data)
-		}
-	}
-}
-
-func watchExits(plasma Plasma) {
-	startedDepositExits := make(chan *contracts.PlasmaMVPStartedDepositExit)
-	startedTransactionExits := make(chan *contracts.PlasmaMVPStartedTransactionExit)
-	challengedExits := make(chan *contracts.PlasmaMVPChallengedExit)
-
-	opts := &bind.WatchOpts{
-		Start:   nil, // latest block
-		Context: context.Background(),
-	}
-	plasma.session.Contract.WatchStartedDepositExit(opts, startedDepositExits)
-	plasma.session.Contract.WatchStartedTransactionExit(opts, startedTransactionExits)
-	plasma.session.Contract.WatchChallengedExit(opts, challengedExits)
-
-	go func() {
-		for depositExit := range startedDepositExits {
-			nonce := depositExit.Nonce.Bytes()
-			key := prefixKey(depositExitPrefix, nonce)
-			plasma.memdb.Put(key, nil)
-		}
-
-		plasma.logger.Info("stopped watching for deposit exits")
-	}()
-
-	go func() {
-		for txExit := range startedTransactionExits {
-			position := plasmaTypes.NewPosition(txExit.Position[0], uint16(txExit.Position[1].Uint64()), uint8(txExit.Position[2].Uint64()), big.NewInt(0))
-			key := prefixKey(transactionExitPrefix, position.Priority().Bytes())
-			plasma.memdb.Put(key, nil)
-		}
-
-		plasma.logger.Info("stopped watching for transaction exits")
-	}()
-
-	go func() {
-		for challengedExit := range challengedExits {
-			if challengedExit.Position[3].Sign() == 0 {
-				position := [3]*big.Int{challengedExit.Position[0], challengedExit.Position[1], challengedExit.Position[2]}
-				key := prefixKey(transactionExitPrefix, calcPriority(position).Bytes())
-				plasma.memdb.Delete(key)
-			} else {
-				key := prefixKey(depositExitPrefix, challengedExit.Position[3].Bytes())
-				plasma.memdb.Delete(key)
-			}
-		}
-
-		plasma.logger.Info("stopped watching for challenged exit")
-	}()
-}
-
-func watchEthBlocks(plasma Plasma, ch <-chan *types.Header) {
 	for header := range ch {
-		plasma.lock.Lock()
+		plasma.Lock()
 		plasma.ethBlockNum = header.Number
-		plasma.lock.Unlock()
+		plasma.Unlock()
 	}
 
-	plasma.logger.Info("Block subscription closed.")
+	plasma.logger.Info("etheruem block header subscription closed")
 }
--- eth/plasma_test.go
+++ eth/plasma_test.go
@@ -2,15 +2,14 @@ package eth
 
 import (
 	"bytes"
-	"crypto/sha256"
 	plasmaTypes "github.com/FourthState/plasma-mvp-sidechain/plasma"
+	"github.com/FourthState/plasma-mvp-sidechain/utils"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/log"
 	"math/big"
 	"os"
-	"reflect"
 	"testing"
 	"time"
 )
@@ -31,25 +30,18 @@ func TestConnection(t *testing.T) {
 
 	t.Logf("Connecting to remote client: %s", clientAddr)
 	client, err := InitEthConn(clientAddr, logger)
-	if err != nil {
-		t.Fatal("Connection Error -", err)
-	}
+	require.NoError(t, err, "connection error")
 
 	_, err = client.accounts()
-	if err != nil {
-		t.Error("Error Retrieving Accounts -", err)
-	}
+	require.NoError(t, err, "error retrieving accounts")
 }
 
 func TestPlasmaInit(t *testing.T) {
 	logger := log.NewTMLogger(os.Stderr)
 	client, err := InitEthConn(clientAddr, logger)
 
-	privKey, _ := crypto.HexToECDSA(operatorPrivKey)
-	_, err = InitPlasma(common.HexToAddress(plasmaContractAddr), privKey, client, logger, 1)
-	if err != nil {
-		t.Fatal("Could not bind contract -", err)
-	}
+	_, err = InitPlasma(common.HexToAddress(plasmaContractAddr), client, 1, logger, false, nil)
+	require.NoError(t, err, "error binding to contract")
 }
 
 func TestSubmitBlock(t *testing.T) {
@@ -57,261 +49,86 @@ func TestSubmitBlock(t *testing.T) {
 	client, _ := InitEthConn(clientAddr, logger)
 
 	privKey, _ := crypto.HexToECDSA(operatorPrivKey)
-	plasma, _ := InitPlasma(common.HexToAddress(plasmaContractAddr), privKey, client, logger, 1)
+	plasma, _ := InitPlasma(common.HexToAddress(plasmaContractAddr), client, 1, logger, true, privKey)
 
 	header := crypto.Keccak256([]byte("blah"))
 	var root [32]byte
 	copy(root[:], header)
-	_, err := plasma.SubmitBlock(root, big.NewInt(0), big.NewInt(0))
-	if err != nil {
-		t.Fatal("Failed block submission -", err)
-	}
+	block := plasmaTypes.Block{root, 0, utils.Big0}
+	err := plasma.SubmitBlock(block)
+	require.NoError(t, err, "block submission error")
 
-	blockNum, err := plasma.session.LastCommittedBlock()
-	if err != nil {
-		t.Fatal("Failed query for the last committed block -", err)
-	}
+	blockNum, err := plasma.contract.LastCommittedBlock(nil)
+	require.NoError(t, err, "failed to query for the last committed block")
 
-	result, err := plasma.session.ChildChain(blockNum)
-	if err != nil {
-		t.Fatal("Failed query for the child chain - ", err)
-	}
+	result, err := plasma.contract.ChildChain(nil, blockNum)
+	require.NoError(t, err, "failed contract plasma chain query")
 
-	if bytes.Compare(result.Root[:], header) != 0 {
-		t.Errorf("Mismatch in block headers. Got: %x. Expected: %x", result, header)
-	}
+	require.Truef(t, bytes.Compare(result.Root[:], header) == 0,
+		"Mismatch in block headers. Got: %x. Expected: %x", result, header)
 }
 
 func TestEthBlockWatching(t *testing.T) {
 	logger := log.NewTMLogger(os.Stderr)
 	client, _ := InitEthConn(clientAddr, logger)
 
-	privKey, _ := crypto.HexToECDSA(operatorPrivKey)
-	plasma, _ := InitPlasma(common.HexToAddress(plasmaContractAddr), privKey, client, logger, 1)
+	plasma, _ := InitPlasma(common.HexToAddress(plasmaContractAddr), client, 1, logger, false, nil)
 
 	// mine a block so that `ethBlockNum` within plasma gets set
 	// sleep after an rpc call to deal with the asynchrony
-	if err := client.rpc.Call(nil, "evm_mine"); err != nil {
-		t.Fatal("Could not mine a block -", err)
-	}
+	err := client.rpc.Call(nil, "evm_mine")
+	require.NoError(t, err, "error mining a block")
 	time.Sleep(1 * time.Second)
 
-	plasma.lock.Lock()
+	plasma.Lock()
 	lastEthBlockNum := plasma.ethBlockNum.Uint64()
-	plasma.lock.Unlock()
+	plasma.Unlock()
 
 	// mine another block that should get caught
-	if err := client.rpc.Call(nil, "evm_mine"); err != nil {
-		t.Fatal("Could not mine a block -", err)
-	}
+	err = client.rpc.Call(nil, "evm_mine")
+	require.NoError(t, err, "error mining a block")
 	time.Sleep(1 * time.Second)
 
-	plasma.lock.Lock()
+	plasma.Lock()
 	currEthBlockNum := plasma.ethBlockNum.Uint64()
-	plasma.lock.Unlock()
-	if currEthBlockNum != lastEthBlockNum+1 {
-		t.Fatalf("EthBlockNum not incremented. Expected: %d, Got: %d",
-			lastEthBlockNum+1, currEthBlockNum)
-	}
+	plasma.Unlock()
+	require.Equalf(t, currEthBlockNum, lastEthBlockNum+1,
+		"EthBlockNum not incremented. Expected: %d, Got: %d", lastEthBlockNum+1, currEthBlockNum)
 }
 
-func TestDepositWatching(t *testing.T) {
+func TestDepositFinalityBound(t *testing.T) {
 	logger := log.NewTMLogger(os.Stderr)
 	client, _ := InitEthConn(clientAddr, logger)
 
 	privKey, _ := crypto.HexToECDSA(operatorPrivKey)
-	plasma, _ := InitPlasma(common.HexToAddress(plasmaContractAddr), privKey, client, logger, 0)
+	// finality bound of 1 ethereum block
+	plasma, _ := InitPlasma(common.HexToAddress(plasmaContractAddr), client, 1, logger, true, privKey)
 
-	nonce, err := plasma.session.DepositNonce()
-	if err != nil {
-		t.Fatalf("Could not query for the next deposit nonce")
-	}
+	// mine a block so that the headers channel is filled with a block
+	err := client.rpc.Call(nil, "evm_mine")
+	require.NoError(t, err, "error mining a block")
+	time.Sleep(1 * time.Second)
+
+	nonce, err := plasma.contract.DepositNonce(nil)
+	require.NoError(t, err, "error querying for the deposit nonce")
 
 	// Deposit 10 eth from the operator
-	plasma.session.TransactOpts.Value = big.NewInt(10)
+	plasma.operatorSession.TransactOpts.Value = big.NewInt(10)
 	operatorAddress := crypto.PubkeyToAddress(privKey.PublicKey)
-	_, err = plasma.session.Deposit(operatorAddress)
-	if err != nil {
-		t.Fatalf("Error sending a deposit tx")
-	}
-	time.Sleep(500 * time.Millisecond)
+	_, err = plasma.operatorSession.Deposit(operatorAddress)
+	require.NoError(t, err, "error sending a deposit tx")
+
+	_, ok := plasma.GetDeposit(nonce)
+	require.False(t, ok, "retrieved a deposit that was inside the finality bound")
+
+	// mine another block so that the deposit falls outside the finality bound
+	err = client.rpc.Call(nil, "evm_mine")
+	require.NoError(t, err, "error mining a block")
+	time.Sleep(1 * time.Second)
 
 	deposit, ok := plasma.GetDeposit(nonce)
-	if !ok {
-		t.Fatal("Deposit not caught")
-	}
+	require.True(t, ok, "could not retrieve a deposit that was deemed final")
 
-	if deposit.Amount.Uint64() != uint64(10) {
-		t.Errorf("Deposit amount incorrect. Expected 10, Got %d", deposit.Amount.Uint64())
-	}
-
-	if !bytes.Equal(operatorAddress[:], deposit.Owner[:]) {
-		t.Errorf("Deposit owner incorrect. Expected %x, Got %x", operatorAddress, deposit.Owner)
-	}
-
-	// check persistence in the db
-	key := prefixKey(depositPrefix, nonce.Bytes())
-	data, err := plasma.memdb.Get(key)
-	if err != nil {
-		t.Fatalf("Deposit not persisted - %s", err)
-	}
-
-	var d plasmaTypes.Deposit
-	err = rlp.DecodeBytes(data, &d)
-	if err != nil {
-		t.Fatalf("Error unmarshaling cache'd deposit - %s", err)
-	}
-
-	if !reflect.DeepEqual(deposit, d) {
-		t.Fatalf("Mismatch in the persisted deposit and `GetDeposit`")
-	}
-}
-
-func TestDepositExitWatching(t *testing.T) {
-	logger := log.NewTMLogger(os.Stderr)
-	client, _ := InitEthConn(clientAddr, logger)
-
-	privKey, _ := crypto.HexToECDSA(operatorPrivKey)
-	plasma, _ := InitPlasma(common.HexToAddress(plasmaContractAddr), privKey, client, logger, 0)
-
-	// deposit and exit
-	nonce, _ := plasma.session.DepositNonce()
-	plasma.session.TransactOpts.Value = big.NewInt(10)
-	_, err := plasma.session.Deposit(crypto.PubkeyToAddress(privKey.PublicKey))
-	if err != nil {
-		t.Fatal("Failed deposit -", err)
-	}
-
-	plasma.session.TransactOpts.Value = big.NewInt(minExitBond)
-	_, err = plasma.session.StartDepositExit(nonce, big.NewInt(0))
-	if err != nil {
-		t.Fatal("Error starting deposit exit -", err)
-	}
-	time.Sleep(500 * time.Millisecond)
-
-	zero := big.NewInt(0)
-	position := plasmaTypes.NewPosition(zero, 0, 0, nonce)
-	exited := plasma.HasTxBeenExited(position)
-
-	if !exited {
-		t.Errorf("Deposit nonce not marked as exited")
-	}
-}
-
-type SpendMsg struct {
-	Blknum0           uint64
-	Txindex0          uint16
-	Oindex0           uint8
-	DepositNum0       uint64
-	Owner0            common.Address
-	Input0ConfirmSigs [][65]byte
-	Blknum1           uint64
-	Txindex1          uint16
-	Oindex1           uint8
-	DepositNum1       uint64
-	OWner1            common.Address
-	Input1ConfirmSigs [][65]byte
-	Newowner0         common.Address
-	Amount0           uint64
-	Newowner1         common.Address
-	Amount1           uint64
-	FeeAmount         uint64
-}
-
-type tx struct {
-	TxList SpendMsg
-	Sigs   [2][]byte
-}
-
-func toEthSignedMessageHash(msg []byte) []byte {
-	buffer := new(bytes.Buffer)
-	buffer.Write([]byte("\x19Ethereum Signed Message:\n32"))
-	buffer.Write(msg)
-	return crypto.Keccak256(buffer.Bytes())
-}
-
-func TestTxExitWatchingAndChallenge(t *testing.T) {
-	logger := log.NewTMLogger(os.Stderr)
-	client, _ := InitEthConn(clientAddr, logger)
-
-	privKey, _ := crypto.HexToECDSA(operatorPrivKey)
-	plasma, _ := InitPlasma(common.HexToAddress(plasmaContractAddr), privKey, client, logger, 0)
-	zero := big.NewInt(0)
-
-	// deposit and spend
-	nonce, _ := plasma.session.DepositNonce()
-	plasma.session.TransactOpts.Value = big.NewInt(10)
-	_, err := plasma.session.Deposit(crypto.PubkeyToAddress(privKey.PublicKey))
-	if err != nil {
-		t.Fatal("Failed deposit -", err)
-	}
-
-	// generate tx
-	var msg SpendMsg
-	msg.DepositNum0 = nonce.Uint64()
-	msg.Newowner0 = crypto.PubkeyToAddress(privKey.PublicKey)
-	msg.Amount0 = 10
-	txList, _ := rlp.EncodeToBytes(msg)
-
-	txHash := crypto.Keccak256(txList)
-	txHash = toEthSignedMessageHash(txHash)
-	sig0, _ := crypto.Sign(txHash, privKey)
-	sigs := [2][]byte{sig0, make([]byte, 65)}
-
-	txBytes, _ := rlp.EncodeToBytes(tx{msg, sigs})
-
-	// submit header. header == merklehash
-	header := sha256.Sum256(txBytes)
-	plasma.session.TransactOpts.Value = nil
-	_, err = plasma.SubmitBlock(header, big.NewInt(1), zero)
-	if err != nil {
-		t.Fatal("Error submitting block -", err)
-	}
-
-	time.Sleep(500 * time.Millisecond)
-
-	// merkleHash == header
-	var data []byte
-	data = append(data, header[:]...)
-	data = append(data, header[:]...)
-	confirmationHash := sha256.Sum256(data)
-	confHash := toEthSignedMessageHash(confirmationHash[:])
-	confirmSignature, _ := crypto.Sign(confHash, privKey)
-
-	plasma.session.TransactOpts.Value = big.NewInt(minExitBond)
-	_, err = plasma.session.StartTransactionExit([3]*big.Int{plasma.blockNum, zero, zero}, txBytes, []byte{}, confirmSignature, zero)
-	if err != nil {
-		t.Fatal("Error starting tx exit -", err)
-	}
-	time.Sleep(500 * time.Millisecond)
-
-	txPos := plasmaTypes.NewPosition(plasma.blockNum, 0, 0, zero)
-	exited := plasma.HasTxBeenExited(txPos)
-	if !exited {
-		t.Errorf("Transaction not marked as exited")
-	}
-
-	// attempt to exit the deposit & challenge
-	depositPos := plasmaTypes.NewPosition(zero, 0, 0, nonce)
-	_, err = plasma.session.StartDepositExit(nonce, zero)
-	if err != nil {
-		t.Fatal("Error exiting deposit -", err)
-	}
-	time.Sleep(500 * time.Millisecond)
-
-	exited = plasma.HasTxBeenExited(depositPos)
-	if !exited {
-		t.Errorf("Deposit not marked as exited after exiting")
-	}
-	plasma.session.TransactOpts.Value = nil
-	_, err = plasma.session.ChallengeExit(depositPos.ToBigIntArray(), [2]*big.Int{plasma.blockNum, zero}, txBytes, []byte{}, confirmSignature)
-	if err != nil {
-		t.Fatal("Error challenging exit -", err)
-	}
-	time.Sleep(500 * time.Millisecond)
-
-	exited = plasma.HasTxBeenExited(depositPos)
-	if exited {
-		t.Errorf("Deposit marked as exited after being challenged")
-	}
+	require.Equal(t, uint64(10), deposit.Amount.Uint64(), "deposit amount mismatch")
+	require.True(t, bytes.Equal(operatorAddress[:], deposit.Owner[:]), "deposit owner mismatch")
 }
