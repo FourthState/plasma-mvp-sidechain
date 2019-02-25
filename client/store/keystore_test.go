44f9eb9e0313cc79fe697639ff49b29543559e26
Colin Axner <colinaxner@berkeley.edu>
2019-02-24 17:15:26 -0800

    upgrade to v0.32.0 sdk

--- client/store/keystore_test.go
+++ client/store/keystore_test.go
@@ -7,25 +7,10 @@ import (
 	"strings"
 	"testing"
 
+	cosmoscli "github.com/cosmos/cosmos-sdk/client"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 )
-
-var currentStdin *bufio.Reader
-
-func init() {
-	currentStdin = bufio.NewReader(os.Stdin)
-}
-
-// overrideStdin allows to temporarily override stdin
-func overrideStdin(newStdin *bufio.Reader) (cleanUp func()) {
-	prevStdin := currentStdin
-	currentStdin = newStdin
-	cleanUp = func() {
-		currentStdin = prevStdin
-	}
-	return cleanUp
-}
 
 // Add, Delete, Update and iterate through accounts
 func TestAccounts(t *testing.T) {
@@ -49,7 +34,7 @@ func TestAccounts(t *testing.T) {
 	}
 
 	for i, n := range cases {
-		cleanUp := overrideStdin(bufio.NewReader(strings.NewReader("test1234\ntest1234\n")))
+		cleanUp := cosmoscli.OverrideStdin(bufio.NewReader(strings.NewReader("test1234\ntest1234\n")))
 		defer cleanUp()
 		_, err := AddAccount(n)
 		require.NoErrorf(t, err, "case %d: failed to add account %s", i, n)
