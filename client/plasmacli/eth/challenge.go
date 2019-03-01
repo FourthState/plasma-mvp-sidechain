255a195fdcc04260010611dfe556c954688f5d40
Colin Axner <colinaxner@berkeley.edu>
2019-03-01 13:02:16 -0800

    add challenge cmd

--- client/plasmacli/eth/challenge.go
+++ client/plasmacli/eth/challenge.go
@@ -1,20 +1,27 @@
 package eth
 
-/*
 import (
 	"fmt"
-	"strconv"
-
 	ks "github.com/FourthState/plasma-mvp-sidechain/client/keystore"
 	"github.com/FourthState/plasma-mvp-sidechain/plasma"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	ethcmn "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	tm "github.com/tendermint/tendermint/rpc/core/types"
+	"math/big"
+	"strconv"
 )
 
 func init() {
 	ethCmd.AddCommand(challengeCmd)
-	challengeCmd.StringP(gasLimitF, "g", "21000", "gas limit for ethereum transaction")
+	challengeCmd.Flags().BoolP(trustNodeF, "t", false, "trust connected full node")
+	challengeCmd.Flags().StringP(gasLimitF, "g", "21000", "gas limit for ethereum transaction")
+	challengeCmd.Flags().String(ownerF, "", "owner of the challenging transaction, required if different from the specified account")
+	challengeCmd.Flags().String(proofF, "", "merkle proof of inclusion")
+	challengeCmd.Flags().StringP(sigsF, "S", "", "confirmation signatures for the challenging transaction")
+	challengeCmd.Flags().StringP(txBytesF, "b", "", "bytes of the challenging transaction")
 	viper.BindPFlags(challengeCmd.Flags())
 }
 
@@ -25,34 +32,67 @@ var challengeCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(3),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// parse positions
-		exitingPos, err := plasma.FromPositionsString(args[0])
+		exitingPos, err := plasma.FromPositionString(args[0])
 		if err != nil {
 			return err
 		}
 
-		challengingPos, err := plasma.FromPositionsString(args[1])
+		challengingPos, err := plasma.FromPositionString(args[1])
 		if err != nil {
 			return err
 		}
 
 		gasLimit, err := strconv.ParseUint(viper.GetString(gasLimitF), 10, 64)
 		if err != nil {
-			return fmt.Error("failed to parse gas limit: { %s }", err)
+			return fmt.Errorf("failed to parse gas limit: { %s }", err)
 		}
 
 		key, err := ks.GetKey(args[2])
 		if err != nil {
-			return fmt.Errof("failed to retrieve account key: { %s }", err)
+			return fmt.Errorf("failed to retrieve account key: { %s }", err)
+		}
+
+		var owner ethcmn.Address
+		if viper.GetString(ownerF) != "" {
+			owner = ethcmn.HexToAddress(viper.GetString(ownerF))
+		} else {
+			owner = crypto.PubkeyToAddress(key.PublicKey)
 		}
 
 		// bind key
 		auth := bind.NewKeyedTransactor(key)
-		transactOpts = bind.TransactOpts{
+		transactOpts := &bind.TransactOpts{
 			From:     auth.From,
 			Signer:   auth.Signer,
 			GasLimit: gasLimit,
 		}
 
+		var txBytes, proof, confirmSignatures []byte
+		if viper.GetBool(trustNodeF) {
+			var result *tm.ResultTx
+			result, confirmSignatures, err = getProof(owner, challengingPos)
+			if err != nil {
+				fmt.Errorf("failed to retrieve exit information: { %s }", err)
+			}
+
+			txBytes = result.Tx
+
+			// flatten proof
+			for _, aunt := range result.Proof.Proof.Aunts {
+				proof = append(proof, aunt...)
+			}
+		}
+
+		txBytes, proof, confirmSignatures, err = parseProof(txBytes, proof, confirmSignatures)
+
+		exitPos := [4]*big.Int{exitingPos.BlockNum, big.NewInt(int64(exitingPos.TxIndex)), big.NewInt(int64(exitingPos.OutputIndex)), exitingPos.DepositNonce}
+		challengePos := [2]*big.Int{challengingPos.BlockNum, big.NewInt(int64(challengingPos.TxIndex))}
+		tx, err := rc.contract.ChallengeExit(transactOpts, exitPos, challengePos, txBytes, proof, confirmSignatures)
+		if err != nil {
+			return fmt.Errorf("failed to send challenge transaction: { %s }", err)
+		}
+
+		fmt.Printf("Sent challenge transaction\nTransaction Hash: 0x%s\n", tx.Hash())
 		return nil
 	},
-}*/
+}
