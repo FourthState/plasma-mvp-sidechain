445407dec42c71db69c280ff75bae3c389d9a3c4
colin axner <colinaxner@berkeley.edu>
2019-03-02 12:28:27 -0800

    Keys Package (#108)
    * refactored keys cmd, made query dir
    
    * fixed directory flag issue, add delete list updated
    
    * completed keys command
    
    * adjusted other commands to use keyname
    
    * changes discussed on discord
    
    * build fix
    
    * forgot to adjust account.go
    
    * rm print, 0x modifier
    
    * changed from rlp to []byte
    
    * redundant string casting
    
    * connect query cmd
    
    * code clean, new format
    
    * update changelog
    
    * R4R: added confirm sig client storage (#116)
    
    * added confirm sig client storage
    
    * added test
    
    * Wip adding tests
    
    * upgrade to v0.32.0 sdk
    
    * added tests
    
    * updated dep
    
    * fix dep issues, upgraded to 0.32.0 sdk
    
    * updated deps
    
    * replace deprecated functions
    
    * deprecate exportState
    
    * rm space between import
    
    * update changelog

--- client/plasmacli/query/account.go
+++ client/plasmacli/query/account.go
@@ -1,34 +1,33 @@
-package cmd
+package query
 
 import (
 	"fmt"
+	ks "github.com/FourthState/plasma-mvp-sidechain/client/store"
 	"github.com/FourthState/plasma-mvp-sidechain/store"
-	"github.com/FourthState/plasma-mvp-sidechain/utils"
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/spf13/cobra"
 	"math/big"
-	"strings"
 )
 
 func init() {
-	rootCmd.AddCommand(balanceCmd)
+	queryCmd.AddCommand(balanceCmd)
 }
 
+// TODO: Change to querying account, add flag for getting all info
 var balanceCmd = &cobra.Command{
-	Use:   "balance <address>",
-	Short: "Query Balances",
+	Use:   "balance <name>",
+	Short: "Query account balance",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx := context.NewCLIContext().WithCodec(codec.New()).WithTrustNode(true)
-		addrStr := utils.RemoveHexPrefix(strings.TrimSpace(args[0]))
-		if !common.IsHexAddress(addrStr) {
-			return fmt.Errorf("address must be provided in hex format")
-		}
+		name := args[0]
 
-		addr := common.HexToAddress(addrStr)
+		addr, err := ks.GetAccount(name)
+		if err != nil {
+			return err
+		}
 
 		res, err := ctx.QuerySubspace(addr.Bytes(), "utxo")
 		if err != nil {
