9c4acb76f1f83e1382e592ccd848b5f48cefb688
kejace <kejace@gmail.com>
2019-04-09 14:04:27 -0400

    Fixed run.sh to handla plasma.toml from outside docker container

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo "setting node to $NODE_URL"
-sed -i "/ethereum_nodeurl = /c\ethereum_nodeurl = $NODE_URL" "$HOME/.plasmad/config/plasma.toml" && \
+echo "wants to set node to $NODE_URL"
+sed "/ethereum_nodeurl = /c\ethereum_nodeurl = $NODE_URL" "$HOME/.plasmad/config/plasma.toml" > "$HOME/.plasmad/config/plasma_with_address.toml"
+cat "$HOME/.plasmad/config/plasma_with_address.toml" > "$HOME/.plasmad/config/plasma.toml"
 cat $HOME/.plasmad/config/plasma.toml
 echo "starting plasmad..."
 plasmad start &
