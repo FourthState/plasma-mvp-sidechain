ce2ad0e01ec880b4c7e7173078755acc2d128faf
kejace <kejace@gmail.com>
2019-04-11 13:58:01 -0400

    Hopefully last travis complaint

--- docker-push.sh
+++ docker-push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push foamspace/plasma-mvp-sidechain
+docker push foamspace/plasma-mvp-sidechain:latest
