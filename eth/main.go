bfccb458c3a545c957a27fa6b4128c95c4dd16a3
Hamdi Allam <hamdi.allam97@gmail.com>
2019-04-26 15:38:46 -0700

    InitEthConn fails if the endpoint is not synced. Refactored errors

--- eth/main.go
+++ eth/main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"encoding/hex"
 	"encoding/json"
-	"fmt"
 	"github.com/FourthState/plasma-mvp-sidechain/utils"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/pkg/errors"
 	"github.com/tendermint/tendermint/libs/log"
 	"math/big"
 )
@@ -21,7 +21,8 @@ type Client struct {
 	logger log.Logger
 }
 
-// Instantiate a connection and bind the go plasma contract wrapper with this client
+// Instantiate a connection and bind the go plasma contract wrapper with this client.
+// Will return an error if the ethereum client is not fully sycned
 func InitEthConn(nodeUrl string, logger log.Logger) (Client, error) {
 	// Connect to a remote etheruem client
 	//
@@ -33,13 +34,36 @@ func InitEthConn(nodeUrl string, logger log.Logger) (Client, error) {
 	}
 	ec := ethclient.NewClient(c)
 
-	return Client{c, ec, logger}, nil
+	// check if the client is synced
+	client := Client{c, ec, logger}
+	if synced, err := client.Synced(); !synced || err != nil {
+		if err != nil {
+			return client, err
+		} else {
+			return client, errors.New("geth endpoint is not fully synced")
+		}
+	}
+
+	return client, nil
+}
+
+func (client Client) Synced() (bool, error) {
+	var res json.RawMessage
+	if err := client.rpc.Call(&res, "eth_syncing"); err != nil {
+		return false, errors.Wrap(err, "rpc")
+	}
+
+	if string(res) != "false" {
+		return false, nil
+	}
+
+	return true, nil
 }
 
 func (client Client) LatestBlockNum() (*big.Int, error) {
 	var hexStr string
 	if err := client.rpc.Call(&hexStr, "eth_blockNumber"); err != nil {
-		return nil, fmt.Errorf("RPC error { %s }", err)
+		return nil, errors.Wrap(err, "rpc")
 	}
 
 	hexStr = utils.RemoveHexPrefix(hexStr)
@@ -51,7 +75,7 @@ func (client Client) LatestBlockNum() (*big.Int, error) {
 
 	hexBytes, err := hex.DecodeString(hexStr)
 	if err != nil {
-		return nil, fmt.Errorf("Hex decoding error: { %s }", err)
+		return nil, errors.Wrap(err, "hex decoding")
 	}
 
 	return new(big.Int).SetBytes(hexBytes), nil
@@ -81,14 +105,13 @@ func (client Client) SubscribeToHeads() (<-chan *types.Header, error) {
 // used for testing when running against a local client like ganache
 func (client Client) accounts() ([]common.Address, error) {
 	var res json.RawMessage
-	err := client.rpc.Call(&res, "eth_accounts")
-	if err != nil {
-		return nil, err
+	if err := client.rpc.Call(&res, "eth_accounts"); err != nil {
+		return nil, errors.Wrap(err, "rpc")
 	}
 
 	var addrs []string
 	if err := json.Unmarshal(res, &addrs); err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "json unmarshaling")
 	}
 
 	// convert to the correct type
