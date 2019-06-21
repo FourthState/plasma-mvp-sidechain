ab8f4be0ade4ec31abcdbf5fd0c4ad7771fd685a
Ilya Ostrovskiy <iostat@users.noreply.github.com>
2019-06-21 16:33:32 -0400

    kubify run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
-echo "wants to set node to $NODE_URL"
-sed "/ethereum_nodeurl = /c\ethereum_nodeurl = $NODE_URL" "$HOME/.plasmad/config/plasma.toml" > "$HOME/.plasmad/config/plasma_with_address.toml"
-cat "$HOME/.plasmad/config/plasma_with_address.toml" > "$HOME/.plasmad/config/plasma.toml"
-cat $HOME/.plasmad/config/plasma.toml
+rm -f nohup.out
+
+if ! [ -z "$NODE_URL" ]; then
+  echo "attempting to override \$NODE_URL to $NODE_URL"
+  sed "/ethereum_nodeurl = /c\ethereum_nodeurl = $NODE_URL" "$HOME/.plasmad/config/plasma.toml" > "$HOME/.plasmad/config/plasma_with_address.toml"
+  cat "$HOME/.plasmad/config/plasma_with_address.toml" > "$HOME/.plasmad/config/plasma.toml"
+fi
+
+echo "plasma.toml is: "
+cat "$HOME/.plasmad/config/plasma.toml"
+
 echo "starting plasmad..."
-plasmad start &
+nohup plasmad start >> nohup.out &
+
 echo "starting plasmacli rest-server"
-plasmacli rest-server &
+nohup plasmacli rest-server >> nohup.out &
+
+if ! [ -z "$KUBERNETES_SERVICE_HOST" ] && ! [ -z "$KUBERNETES_SERVICE_PORT" ]; then
+  echo "looks like we're running in kube, will tail nohup.out while waiting for any subprocess to finish"
+  cat nohup.out &
+  wait -n
+else
+  echo "looks like we're not running in kube, exiting immediately"
+fi
