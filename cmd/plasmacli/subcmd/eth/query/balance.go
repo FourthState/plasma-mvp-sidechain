b224cc891e0a4ec56f1451068a4b602bf02e7d84
Hamdi Allam <hamdi.allam97@gmail.com>
2019-06-24 12:42:14 -0700

    flags should be bound for balance.go

--- cmd/plasmacli/subcmd/eth/query/balance.go
+++ cmd/plasmacli/subcmd/eth/query/balance.go
@@ -23,6 +23,7 @@ Usage:
 	plasmacli eth query balance --address <address>`,
 	Args: cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		viper.BindPFlags(cmd.Flags())
 		var addr ethcmn.Address
 
 		if viper.GetString(addrF) != "" {
