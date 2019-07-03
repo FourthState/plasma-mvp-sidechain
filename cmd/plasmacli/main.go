832e3e4bf147e830cf715a7827000770f3f74041
Hamdi Allam <hamdi.allam97@gmail.com>
2019-07-02 21:32:18 -0700

    remove flag definition in main

--- cmd/plasmacli/main.go
+++ cmd/plasmacli/main.go
@@ -3,12 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/FourthState/plasma-mvp-sidechain/cmd/plasmacli/subcmd"
-	tmcli "github.com/tendermint/tendermint/libs/cli"
 	"os"
-)
-
-const (
-	FlagHome = tmcli.HomeFlag
 )
 
 func main() {
