c0441e4a9bb1f7eed9cac48e87df9ab1d53349c0
Hamdi Allam <hamdi.allam97@gmail.com>
2020-03-11 14:10:43 -0700

    finish of the tx cmd

--- cmd/plasmacli/subcmd/query/tx.go
+++ cmd/plasmacli/subcmd/query/tx.go
@@ -1,14 +1,13 @@
 package query
 
 import (
+	"encoding/hex"
 	"fmt"
 	"github.com/FourthState/plasma-mvp-sidechain/client"
-	"github.com/FourthState/plasma-mvp-sidechain/cmd/plasmacli/flags"
 	"github.com/FourthState/plasma-mvp-sidechain/plasma"
 	"github.com/FourthState/plasma-mvp-sidechain/utils"
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"strings"
 )
 
@@ -20,15 +19,13 @@ func TxCommand() *cobra.Command {
 var txCmd *cobra.Command = &cobra.Command{
 	Use:   "tx <txHash/position>",
 	Short: "Query for information about a single transaction or transaction output",
-	Long: `Query for information about a single transaction or transaction output.
-If --verbose is set, additional information about the transaction will also be displayed`,
-	Args: cobra.ExactArgs(1),
-	RunE: func(cmd *Cobra.Command, args []string) error {
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx := context.NewCLIContext()
 		cmd.SilenceUsage = true
+		arg := strings.TrimSpace(args[0])
 
 		// argument validation
-		arg := utils.RemoveHexPrefix(strings.TrimSpace(args[0]))
 		if pos, err := plasma.FromPositionString(arg); err != nil {
 			// utxo position
 			txOutput, err := client.TxOutput(ctx, pos)
@@ -36,8 +33,43 @@ If --verbose is set, additional information about the transaction will also be d
 				fmt.Printf("Error quering transaction output %s: %s\n", pos, err)
 				return err
 			}
+
+			// display output information
+			fmt.Printf("TxOutput: %s\n", pos)
+			fmt.Printf("Confirmation Hash: 0x%x\n", txOutput.ConfirmationHash)
+			fmt.Printf("Transaction Hash: 0x%x\n", txOutput.TxHash)
+			fmt.Printf("Spent: %v\n", txOutput.Spent)
+			if txOutput.Spent {
+				fmt.Printf("Spending Tx Hash: 0x%x\n", txOutput.SpenderTx)
+			}
+
 		} else {
 			// transaction hash
+			txHash, err := hex.DecodeString(utils.RemoveHexPrefix(arg))
+			if err != nil {
+				fmt.Printf("Error decoding tx hash hex-encoded string: %s\n", err)
+				return err
+			}
+
+			tx, err := client.Tx(ctx, txHash)
+			if err != nil {
+				fmt.Printf("Error queyring trasnsaction hash: 0x%x\n", txHash)
+				return err
+			}
+
+			// display transaction information
+			fmt.Printf("Block %d, Tx Index: %d\n", tx.Position.BlockNum, tx.Position.TxIndex)
+			fmt.Printf("TxHash: 0x%x\n", txHash)
+			fmt.Printf("Confirmation Hash: 0x%x\n", tx.ConfirmationHash)
+			// we expect the length of `SpenderTx` and `Spent` to be the same
+			for i, spender := range tx.SpenderTxs {
+				spent := tx.Spent[i]
+				fmt.Println("Ouput %d:\n\tSpent: %v\n", i, spent)
+				if spent {
+					// keep the indentendation
+					fmt.Printf("\tSpender Tx: 0x%x\n", spender)
+				}
+			}
 		}
 
 		return nil
